b272b78493d36701506f871d6cf2adb7b30da1a0
Andria Dawson <andria.dawson@gmail.com>
2020-05-19 13:46:59 -0600

    cleaning up pull_pullen.R

--- R/pull_pollen.R
+++ R/pull_pollen.R
@@ -1,13 +1,24 @@
 library(neotoma)
 library(analogue)
 library(Bchron)
+library(ggplot2)
+library(data.table)
 
 # USA Contiguous albers equal area
-proj_out <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+proj_out <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 
+             +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
 
+# limits
+lat_hi  = 49
+lat_lo  = 44
+long_west = -100
+long_east = -70
+
+# version
+version = '1.0'
 
 # get datasets withing bounded region
-all.datasets <- get_dataset(loc = c(-100, 44, -70, 49),
+all.datasets <- get_dataset(loc = c(long_west, lat_lo, long_east, lat_hi),
                             datasettype = "pollen")
 
 map <- map_data("world")
@@ -18,21 +29,30 @@ ggplot(data = data.frame(map), aes(long, lat)) +
   xlab("Longitude West") + 
   ylab("Latitude North") +
   coord_map(projection = "albers",
-            lat0 = 44,
-            lat1 = 49, 
-            xlim = c(-100, -70),
-            ylim = c(44, 49))
+            lat0 = lat_lo,
+            lat1 = lat_hi, 
+            xlim = c(long_west, long_east),
+            ylim = c(lat_lo, lat_hi))
 
-# if (file.exists("../data/all.downloads.rds")) {
-#   all.downloads <- readRDS("../data/all.downloads.rds")
-# } else {
-  all.downloads <- get_download(all.datasets, verbose = FALSE)
-  saveRDS(all.downloads, "../data/all.downloads.rds")
-# }
+# check if existing download contains all datasets
+if (file.exists("data/all.downloads.RDS")) {
+  all.downloads.old <- readRDS("data/all.downloads.RDS")
+  if (all(names(all.downloads.old) == names(all.datasets))){
+    all.downloads <- all.downloads.old
+  }
+} else {
+  all.downloads <- get_download(all.datasets, 
+                                verbose = FALSE)
+  saveRDS(all.downloads, "data/all.downloads.RDS")
+}
 
+# standardize the pollen taxonomy
 all.cores <- compile_taxa(all.downloads, "P25")
+
+# convert list to data frame
 compiled.cores  <- compile_downloads(all.cores)
 
+# translate any dates in radiocarbon years to calendar years
 radio.years <- (compiled.cores$date.type %in% "Radiocarbon years BP") &
   (compiled.cores$age > 71 ) &
   (compiled.cores$age < 46401)
@@ -45,12 +65,13 @@ calibrated <- BchronCalibrate(compiled.cores$age[radio.years],
 wmean.date <- function(x) sum(x$ageGrid*x$densities / sum(x$densities))
 
 compiled.cores$age[radio.years] <- sapply(calibrated, wmean.date)
-# compiled.cores <- na.omit(compiled.cores)
 
 hist(compiled.cores$age)
 
+# subset to include only samples between 0 and 200 YBP
 compiled.cores.sub <- subset(compiled.cores, subset=(age>0)&(age<200))
 
+# visualize subsetted data
 map <- map_data("world")
 ggplot(data = data.frame(map), aes(long, lat)) + 
   geom_polygon(aes(group=group), color = "steelblue", alpha = 0.2) +
@@ -59,55 +80,30 @@ ggplot(data = data.frame(map), aes(long, lat)) +
   xlab("Longitude West") + 
   ylab("Latitude North") +
   coord_map(projection = "albers",
-            lat0 = 44,
-            lat1 = 53, 
-            xlim = c(-100, -60),
-            ylim = c(44, 49))
+            lat0 = lat_lo,
+            lat1 = lat_hi, 
+            xlim = c(long_west, long_east),
+            ylim = c(lat_lo, lat_hi))
 
 
+# convert projection to proj_out
 sp::coordinates(compiled.cores) <- ~long+lat
 sp::proj4string(compiled.cores) <- sp::CRS('+init=epsg:4326')
 albers <- sp::spTransform(compiled.cores, proj_out)
 xy = data.frame(coordinates(albers))
 colnames(xy) = c('x', 'y')
 
+# construct data frame with re-projected coordinates
 compiled.cores = data.frame(xy, compiled.cores)
 compiled.cores = compiled.cores[,which(colnames(compiled.cores)!= 'optional')]
 
+# 
 compiled.counts = compiled.cores[,13:ncol(compiled.cores)]
 compiled.props  = compiled.cores[,13:ncol(compiled.cores)]/rowSums(compiled.cores[,13:ncol(compiled.cores)])
 compiled.locs   = compiled.cores[,c('x', 'y')]
 
-# library(maptools)
-# source('../source/pie_maps.R')
-# 
-# xlim = c(-100, -60)
-# ylim = c(44, 49)
-# 
-# xlo = min(xlim)
-# xhi = max(xlim)
-# ylo = min(ylim)
-# yhi = max(ylim)
-# 
-# shift = 24000
-# 
-# par(mfrow=c(1,1))
-# pieMap(proportions = compiled.props,
-#        centers  = compiled.locs,
-#        restrict = FALSE,
-#        inputRestricted = FALSE,
-#        xlim   = c(xlo+shift, xhi-shift),
-#        ylim   = c(ylo+shift, yhi-shift),
-#        radius = 25000,
-#        scale  = 1,
-#        xlab   = 'x',
-#        ylab   = 'y',
-#        add_legend=FALSE,
-#        main_title='',
-#        cont_shp=cont_shp)
-
-
-# taxa.keep = c('Acer', 'Alnus','Betula', 'Fagus', 'Ostrya.Carpinus', 'Picea', 'Pinus', 'Quercus', 'Tsuga', 'Ulmus')
+# taxa.keep = c('Acer', 'Alnus','Betula', 'Fagus', 'Ostrya.Carpinus', 
+#               'Picea', 'Pinus', 'Quercus', 'Tsuga', 'Ulmus')
 taxa.keep = c('Acer', 'Alnus','Betula', 'Cyperaceae', 'Fagus', 'Ostrya.Carpinus', 
               'Picea', 'Pinus', 'Quercus', 'Tsuga', 'Ulmus')
 taxa.nontree = c('Other', 'Prairie.Forbs', 'Poaceae')
@@ -119,31 +115,10 @@ compiled.counts.sub = data.frame(compiled.counts[, which(colnames(compiled.count
                  Other = rowSums(compiled.other, na.rm=TRUE))
 
 compiled.props.sub = compiled.counts.sub/rowSums(compiled.counts.sub)
-
-
 compiled.counts.locs = data.frame(compiled.locs, compiled.counts.sub)
 
-library(data.table)
 dat = as.data.table(compiled.counts.locs)[, lapply(.SD, sum, na.rm=TRUE), by = list(x, y)]
 dat[,3:ncol(dat)] = round(dat[,3:ncol(dat)])
-
-
-# mdat = melt(compiled.counts.locs, id.vars=c('x', 'y'))
-# dat  = dcast(mdat, x + y ~ variable, function(x) sum(x, na.rm=TRUE))
-# dat[,3:ncol(dat)] = round(dat[,3:ncol(dat)])
-
 N_taxa = ncol(compiled.counts.sub)
-saveRDS(dat, paste0('../data/', N_taxa, 'taxa_dat.RDS'))
 
-# map <- map_data("world")
-# ggplot(data = data.frame(map), aes(long, lat)) + 
-#   geom_polygon(aes(group=group), color = "steelblue", alpha = 0.2) +
-#   geom_point(data = get_site(all.datasets),
-#              aes(x = long, y = lat), color = 2) +
-#   xlab("Longitude West") + 
-#   ylab("Latitude North") +
-#   coord_map(projection = "albers",
-#             lat0 = 44,
-#             lat1 = 53, 
-#             xlim = c(-100, -60),
-#             ylim = c(44, 49))
+saveRDS(dat, paste0('data/', N_taxa, 'taxa_pollen_dat_v', version, '.RDS'))
