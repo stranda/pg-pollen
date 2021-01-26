c79fb7dff4816e77f0918f7694412360a6385983
Alissa Brown <abrown324@gmail.com>
2021-01-26 10:27:02 -0600

    update

--- R/pull_pollen_ST.R
+++ R/pull_pollen_ST.R
@@ -8,22 +8,25 @@ library(sp)
 library(dplyr)
 library(raster)
 
-# setwd('C:/Users/abrow/Documents/pg-pollen')
+setwd('C:/Users/abrow/Documents/pg-pollen')
+version = 'Dec15'
 
 # READ SHAPEFILES AND ESTABLISH SPATIAL DOMAIN
 # USA Contiguous albers equal area
-proj_out <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 
-+ellps=GRS80 +datum=NAD83 +units=m +no_defs'
+# OLD PROJECTION
+# proj_out <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 
+# +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
 
 # Get raster masks and spatial domain you want to use
 # (Adam Smith's .tif from: NSF_ABI_2018_2021/data_and_analyses/green_ash/study_region/!study_region_raster_masks)
 stack <- stack('data/map-data/study_region_daltonIceMask_lakesMasked_linearIceSheetInterpolation.tif')
+proj <- proj4string(stack)
 
 # Adam's spatial domain
 coords <- data.frame(x = c(stack@extent@xmin,stack@extent@xmax),
                      y = c(stack@extent@ymin,stack@extent@ymax))
 sp::coordinates(coords) <- ~x + y
-sp::proj4string(coords) <- CRS(proj_out)
+sp::proj4string(coords) <- CRS(proj)
 coords <- sp::spTransform(coords, CRS("+init=epsg:4326"))
 
 # limits
@@ -31,9 +34,6 @@ lat_hi  = coords@coords[2,2]
 lat_lo  = coords@coords[1,2]
 long_west = coords@coords[1,1]
 long_east = coords@coords[2,1]
-
-# version
-version = 'Oct12'
 
 # DOWNLOAD DATA withing bounded region from NEOTOMA
 all.datasets <- get_dataset(loc = c(long_west, lat_lo, long_east, lat_hi),
@@ -66,11 +66,11 @@ if (file.exists("data/all.downloads.RDS")) {
 }
 
 # are there any age NAs?
-names <- names(all.cores)
-test <- lapply(names, function(x) anyNA(all.cores$x$chronologies$`palEON-STEPPS`))
+names <- names(all.downloads)
+test <- lapply(names, function(x) anyNA(all.downloads$x$chronologies$`palEON-STEPPS`))
 test <- data.frame(test)
 any(test == TRUE)
-test <- lapply(names, function(x) anyNA(all.cores$x$chronologies$`NAPD 1`))
+test <- lapply(names, function(x) anyNA(all.downloads$x$chronologies$`NAPD 1`))
 test <- data.frame(test)
 any(test == TRUE)
 
@@ -85,6 +85,10 @@ all.cores <- compile_taxa(all.downloads, "P25")
 
 # convert list to data frame
 compiled.cores  <- compile_downloads(all.cores)
+
+# until there's an answer as to why we get age NAs after running 'compile_downloads'...
+# for now just remove rows with age NAs
+compiled.cores <- compiled.cores[!is.na(compiled.cores$age), ]
 
 # translate any dates in radiocarbon years to calendar years
 radio.years <- (compiled.cores$date.type %in% "Radiocarbon years BP") &
@@ -107,20 +111,20 @@ hist(compiled.cores$age)
 map <- map_data("world")
 ggplot(data = data.frame(map), aes(long, lat)) + 
   geom_polygon(aes(group=group), color = "steelblue", alpha = 0.2) +
-  geom_point(data = compiled.cores,
-             aes(x = long, y = lat), color = 2) +
+  geom_point(data = neg_ages,
+             aes(x = long, y = lat), color = 2, size = 2) +
   xlab("Longitude West") + 
   ylab("Latitude North") +
   coord_map(projection = "albers",
             lat0 = lat_lo,
             lat1 = lat_hi, 
-            xlim = c(long_west, long_east),
+            xlim = c(long_west, -59),
             ylim = c(lat_lo, lat_hi))
 
-# convert projection to proj_out
+# convert projection to proj
 sp::coordinates(compiled.cores) <- ~long+lat
 sp::proj4string(compiled.cores) <- sp::CRS('+init=epsg:4326')
-albers <- sp::spTransform(compiled.cores, proj_out)
+albers <- sp::spTransform(compiled.cores, proj)
 xy = data.frame(coordinates(albers))
 colnames(xy) = c('x', 'y')
 
@@ -132,42 +136,28 @@ compiled.cores = compiled.cores[,which(colnames(compiled.cores)!= 'optional')]
 taxa.nontree <- c('Other', 'Prairie.Forbs', 'Poaceae')
 tree.cores <- compiled.cores[, which(!(colnames(compiled.cores) %in% taxa.nontree))]
 
-# remove sites with no pollen counts (IS THIS RIGHT????)
-tree.cores[,13:35]=apply(tree.cores[,13:35], MARGIN = c(1,2), 
-                         function(x) if (is.na(x)){x=0}else{x})
+# ASK ANDRIA ABOUT THIS CODE BELOW
+# remove sites with no tree pollen counts
+# i.e., rows that contain only zeros/NAs across all taxa
+tree.cores$sum <- apply(tree.cores[,13:35], 1, function(x) sum(x, na.rm=TRUE))
+tree.cores <- tree.cores[tree.cores$sum > 0, ]
 
-# standardize pollen counts so every site has same total # pollen grains across taxa
-# (only need to do this if you're analyzing a subset of all taxa in dataset)
-stand.cores <- tree.cores
-# stand.cores[ ,13:ncol(stand.cores)] <- t(apply(stand.cores[ ,13:ncol(stand.cores)],
-#                                          1,
-#                                          function(x) x/sum(x, na.rm=TRUE)*500))
-
-# stand.cores[ ,13:ncol(stand.cores)] <- t(apply(stand.cores[ ,13:ncol(stand.cores)],
-#                                                1,
-#                                                function(x) if (sum(x)==0) {x} else {x/sum(x, na.rm=TRUE)*500}))
-
-# check your work
-# all(round(rowSums(stand.cores[, 13:ncol(stand.cores)], na.rm=TRUE)) == 500) 
-
-# create time chunks and add to dataframe
-# dat.cut <- cut(stand.cores$age, breaks = c(min(stand.cores$age, na.rm = TRUE), seq(1000, 21000, by = 1000)),
-#                labels = FALSE)
-# stand.cores$cut <- dat.cut
-# stand.cores <- stand.cores[!is.na(stand.cores$cut),]
-
-####
 # ASSIGN TIME CHUNKS
-####
 
-time_bins = seq(0, 21000, by=990)
+# QUESTIONS
+# WHY ARE THERE AGES LESS THAN -60? HOW DO WE BIN NEGATIVE AGES?
+# PLACE ALL NEGATIVE AGES IN SAME BIN AS TIME = 0?
+# DO WE USE ALL DATE TYPES? ARE DIFF'T DATE TYPES COMPARABLE AFTER 
+# RUNNING THE BCHRON CODE? i still don't understand this chunk of code
+# For now, just bin everything that's <0 together
+
+time_bins = c(min(tree.cores$age), seq(0, 21000, by=990))
 N_times = length(time_bins)-1
 
-test.cut <- cut(stand.cores$age, 
-                breaks = time_bins)
-test <- stand.cores
+test.cut <- cut(tree.cores$age, include.lowest = TRUE, breaks = time_bins)
+test <- tree.cores
 test$cut <- as.integer(test.cut)
-test <- test[!is.na(test$cut),]
+test <- test[!is.na(test$cut),] # remove rows with records >21k years old
 
 # assign unique ID to each distinct x, y coordinate
 xyid <- test[,c('x','y')] %>% distinct()
@@ -176,14 +166,22 @@ xyid$id <- as.character(seq(1, nrow(xyid), by = 1))
 # separate dataframe into list of dataframes, one for each time chunk, remove excess columns
 time <- split(test, f = test$cut)
 
+# QUESTION: NOW THAT WE AREN'T TESTING THE MODEL ANYMORE, SHOULD WE ADD MORE TAXA?
+# we'll need to rationalize why we used the taxa we did
+# FOR NOW, USE TAXA WITH >200,000 TOTAL POLLEN COUNT
 # extract pollen data from specified taxa
-taxa.keep = c('Acer', 'Alnus','Betula', 'Cyperaceae', 'Fagus', 'Fraxinus',
-              'Ostrya.Carpinus', 'Picea', 'Pinus', 'Quercus', 'Tsuga', 'Ulmus')
+# and combine the other tree taxa into 'other' column
+
+props <- tree.cores[,13:35]/rowSums(tree.cores[,13:35], na.rm = TRUE)
+
+summ <- data.frame(apply(tree.cores[,13:35], 2, function(x) sum(x, na.rm=TRUE)))
+summ$gtlt <- ifelse(summ$pollen_sum<200000,"less","greater")
+taxa.keep <- rownames(summ[summ$pollen_sum >= 200000, ])
 taxa.nontree = c('Other', 'Prairie.Forbs', 'Poaceae')
 
 for (i in 1:N_times){
   compiled.meta = time[[i]][,c('x', 'y')]
-  compiled.counts = time[[i]][,13:(ncol(time[[i]])-1)]
+  compiled.counts = time[[i]][,13:35]
   compiled.other = compiled.counts[, which((!(colnames(compiled.counts) %in% taxa.keep)) & 
                                           (!(colnames(compiled.counts) %in% taxa.nontree)))]
   compiled.counts.sub = data.frame(compiled.counts[, which(colnames(compiled.counts) %in% taxa.keep)],
@@ -204,36 +202,26 @@ for(i in 1:N_times){
   time_sum[[i]] <- left_join(xyid, time_sum[[i]], by = c('x','y'))
 }
 
-# DO NOT CHANGE NAS TO ZEROS! KEEP THEM AS NA
-# change NAs to zeros
-# for(i in 1:N_times){
-#   time_sum[[i]][,c(4:ncol(time_sum[[i]]))] <- apply(time_sum[[i]][,c(4:ncol(time_sum[[i]]))], c(1,2), 
-#                                   function(x) 
-#                                     if (is.na(x)){x=0}
-#                                   else{x})
-# }
-
-# use rounding to get integer counts
-counts <- time_sum
+# convert numeric pollen counts to integer class
 for(i in 1:N_times){
-  counts[[i]][,c(4:ncol(time_sum[[i]]))] <- round(counts[[i]][,c(4:ncol(time_sum[[i]]))])
+  time_sum[[i]][,4:17] <- apply(time_sum[[i]][,4:17], c(1,2), as.integer)
 }
 
 # extract site coordinates
-locs <- counts[[1]][,c('x','y')]
+locs <- time_sum[[1]][,c('x','y','id')]
 N_locs = nrow(locs)
 N_taxa = length(taxa.keep) + 1
-locs = cbind(locs, id = seq(1, N_locs))
 
 dat_array = array(0, dim = c(N_locs, N_taxa, N_times))
 for (i in 1:N_times){
-  for (j in 1:nrow(counts[[i]])){
-    site_id = as.numeric(counts[[i]]$id[j])
-    dat_array[site_id,,i] = as.numeric(unname(counts[[i]][j,4:(4+N_taxa-1)]))
+  for (j in 1:N_locs){
+    site_id = as.numeric(time_sum[[i]]$id[j])
+    dat_array[site_id,,i] = as.numeric(unname(time_sum[[i]][j,4:(4+N_taxa-1)]))
   }
 }
 
 locs <- locs[,c('x','y')]
 
-saveRDS(dat_array, paste0('data/', 'pollen_dat_v', version, '.RDS'))
-saveRDS(locs, paste0('data/', 'pollen_locs_v', version, '.RDS'))
+saveRDS(dat_array, paste0('data/', 'pollen_dat_', version, '.RDS'))
+saveRDS(locs, paste0('data/', 'pollen_locs_', version, '.RDS'))
+saveRDS(taxa.keep, paste0('data/', 'pollen_taxa_', version, '.RDS'))
