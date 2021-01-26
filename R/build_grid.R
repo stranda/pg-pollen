880194a164f0c27fb2c10b47e4343d270e3939ba
Alissa Brown <abrown324@gmail.com>
2021-01-26 10:33:20 -0600

    update

--- R/build_grid.R
+++ R/build_grid.R
@@ -1,58 +1,52 @@
-library(reshape2)
-library(rdist)
-library(rgeos)
-library(ggplot2)
-library(sp)
-library(rgdal)
-library(raster)
-library(fields)
 
-#### READ IN DATA ####
-dat = readRDS('data/12taxa_pollen_dat_v1.0.RDS')
+require(raster)
+require(sp)
 
-# note that locations were scaled to fit the model
-# unscaling to think in meters, then will rescale again before prediction
-locs_pollen <- data.frame(dat[,c('x', 'y')] )
+setwd('C:/Users/abrow/Documents/pg-pollen')
+version <- 'Dec15'
 
-taxa = colnames(dat)[!(colnames(dat) %in% c('x', 'y'))]
-y = dat[,..taxa]
+# READ IN DATA
+dat <- readRDS(paste0('output/polya-gamma-dat_', version, '.RDS'))
+locs_pollen <- dat$locs
 
-#### CONSTRUCT GRID ####
-# function to construct a raster grid
-# take in bounding box for grid, resolution in m, and projection
-build_grid <- function(veg_box, resolution = 24000, proj = '+init=epsg:3175') {
-  raster::raster(xmn = veg_box[1],
-                 xmx = veg_box[3],
-                 ymn = veg_box[2],
-                 ymx = veg_box[4],
-                 resolution = resolution,
-                 crs = proj)
-}
+# Get spatial domain and projection you want to use
+# (Adam Smith's .tif from: NSF_ABI_2018_2021/data_and_analyses/green_ash/study_region/!study_region_raster_masks)
+stack <- stack('data/map-data/study_region_daltonIceMask_lakesMasked_linearIceSheetInterpolation.tif')
+proj <- proj4string(stack)
 
-bbox_tran <- function(x, coord_formula = '~ x + y', from, to) {
-  
-  sp::coordinates(x) <- formula(coord_formula)
-  sp::proj4string(x) <- sp::CRS(from)
-  bbox <- as.vector(sp::bbox(sp::spTransform(x, CRSobj = sp::CRS(to))))
-  return(bbox)
-}
+# Adam's spatial domain
+coords <- data.frame(x = c(stack@extent@xmin,stack@extent@xmax),
+                     y = c(stack@extent@ymin,stack@extent@ymax))
+sp::coordinates(coords) <- ~x + y
+sp::proj4string(coords) <- CRS(proj)
+pol_bbox <- bbox(coords)
 
-# get bounding box from pollen record coordinates
-pol_box <- bbox_tran(locs_pollen, '~ x + y',
-                     proj_out,
-                     proj_out)
-xlim = c(pol_box[1]-24000, pol_box[3]+24000)
-ylim = c(pol_box[2]-24000, pol_box[4]+24000)
+# construct the grid using Adam's bounding box, 40km resolution, Adam's projection
+reconst_grid <- raster::raster(xmn = pol_bbox[1,1],
+                               xmx = pol_bbox[1,2],
+                               ymn = pol_bbox[2,1],
+                               ymx = pol_bbox[2,2],
+                               resolution = 60000,
+                               crs = proj)
 
-# build the raster grid
-# 40 km grid cells
-reconst_grid <- build_grid(pol_box,
-                           resolution = 60000,
-                           proj = proj_out)
-
-# want to work with a data frame not a raster
-reconst_grid = as.data.frame(reconst_grid, xy=TRUE)
-
+# want to work with a data frame, not a raster
+reconst_grid = raster::as.data.frame(reconst_grid, xy=TRUE)
 locs_grid = reconst_grid[,1:2]
 
-saveRDS(locs_grid, 'data/grid.RDS')
+saveRDS(locs_grid, paste0('data/grid_', version, '.RDS'))
+
+
+# visualize grid
+na_shp <- readOGR("data/map-data/NA_States_Provinces_Albers.shp", "NA_States_Provinces_Albers")
+na_shp <- sp::spTransform(na_shp, proj)
+cont_shp <- subset(na_shp,
+                   (NAME_0 %in% c("United States of America", "Mexico", "Canada")))
+lake_shp <- readOGR("data/map-data/Great_Lakes.shp", "Great_Lakes")
+lake_shp <- sp::spTransform(lake_shp, proj)
+
+ggplot(data = locs_grid, aes(x = x, y = y)) + 
+  geom_path(data = cont_shp, aes(x = long, y = lat, group = group)) +
+  geom_path(data = lake_shp, aes(x = long, y = lat, group = group)) +
+  geom_point() +
+  coord_equal()
+
