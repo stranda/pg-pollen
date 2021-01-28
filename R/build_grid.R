079395bf235e13275432d00f7a3aaec25df2772c
Andria Dawson <andria.dawson@gmail.com>
2021-01-28 09:35:07 -0700

    cropping grid to only land

--- R/build_grid.R
+++ R/build_grid.R
@@ -14,6 +14,21 @@ locs_pollen <- dat$locs
 stack <- stack('data/map-data/study_region_daltonIceMask_lakesMasked_linearIceSheetInterpolation.tif')
 proj <- proj4string(stack)
 
+# build a raster that identifies gric cells that were land at any point since LGM
+# 1 if land, 0 if not land
+land = stack[[1]]
+nrow = dim(land)[1]
+ncol = dim(land)[2]
+for (i in 1:nlayers(stack)){
+  print(paste0("layer :", i))
+  raster_sub = stack[[i]]
+  land <- overlay(land, raster_sub, fun = function(x, y) {
+    x[!is.na(y[])] <- 1
+    return(x)
+  })
+}
+
+
 # Adam's spatial domain
 coords <- data.frame(x = c(stack@extent@xmin,stack@extent@xmax),
                      y = c(stack@extent@ymin,stack@extent@ymax))
@@ -28,10 +43,25 @@ reconst_grid <- raster::raster(xmn = pol_bbox[1,1],
                                ymx = pol_bbox[2,2],
                                resolution = 60000,
                                crs = proj)
+vals = rep(1, ncell(reconst_grid))
+reconst_grid = setValues(reconst_grid, vals)
+
+land_resamp <- resample(land,reconst_grid,method='bilinear')
+
+# land_array = as.array(land_resamp)[,,1]
+
+extent(land_resamp) == extent(reconst_grid)
+
+reconst_grid_land <- overlay(reconst_grid, land_resamp, fun = function(x, y) {
+  x[is.na(y[])] <- NA
+  return(x)
+})
 
 # want to work with a data frame, not a raster
-reconst_grid = raster::as.data.frame(reconst_grid, xy=TRUE)
-locs_grid = reconst_grid[,1:2]
+reconst_land_df = raster::as.data.frame(reconst_grid_land, xy=TRUE)
+reconst_land_df = reconst_land_df[which(!is.na(reconst_land_df$layer)),]
+
+locs_grid = reconst_land_df[,1:2]
 
 saveRDS(locs_grid, paste0('data/grid_', version, '.RDS'))
 
