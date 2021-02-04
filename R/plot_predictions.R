c516318ec1860a0f685a0e895217de5fcc7cf0d8
Alissa Brown <abrown324@gmail.com>
2021-02-04 10:08:12 -0600

    added code to create rel prop vs. predictions scatterplot

--- R/plot_predictions.R
+++ R/plot_predictions.R
@@ -7,6 +7,7 @@ require(fields)
 require(rgdal)
 require(raster)
 require(enmSdm)
+require(rgeos)
 
 setwd('C:/Users/abrow/Documents/pg-pollen')
 version <- 'Dec15'
@@ -66,23 +67,23 @@ ylim = c(grid_box[2], grid_box[4])
 
 #### PREPARE RAW DATAFRAMES ####
 # create list of dataframes, grouped by time period
-dat_list <- list()
-for(i in 1:n_times){
-  dat_list[[i]] <- cbind(dat$locs, data.frame(dat$y[,,i]))
-  dat_list[[i]] <- pivot_longer(dat_list[[i]], cols = 3:ncol(dat_list[[i]]),
-                                 names_to = 'taxon_num', values_to = 'count')
-  dat_list[[i]]$taxon_num <- as.integer(substr(dat_list[[i]]$taxon_num, start = 2, 
-                                                stop = nchar(dat_list[[i]]$taxon_num)))
-  dat_list[[i]] <- left_join(dat_list[[i]], taxa, by = 'taxon_num')
-  dat_list[[i]] <- dat_list[[i]] %>% dplyr::select(-taxon_num)
-}
-
-# create list of taxon-specific dataframes within time-specific list items
-dat_list <- lapply(dat_list, function(x) split(x, f = x$taxon))
-for(i in 1:n_times){
-  dat_list[[i]] <- lapply(dat_list[[i]], 
-                           function(x) x[!(names(x) %in% c('taxon'))])
-}
+# dat_list <- list()
+# for(i in 1:n_times){
+#   dat_list[[i]] <- cbind(dat$locs, data.frame(dat$y[,,i]))
+#   dat_list[[i]] <- pivot_longer(dat_list[[i]], cols = 3:ncol(dat_list[[i]]),
+#                                  names_to = 'taxon_num', values_to = 'count')
+#   dat_list[[i]]$taxon_num <- as.integer(substr(dat_list[[i]]$taxon_num, start = 2, 
+#                                                 stop = nchar(dat_list[[i]]$taxon_num)))
+#   dat_list[[i]] <- left_join(dat_list[[i]], taxa, by = 'taxon_num')
+#   dat_list[[i]] <- dat_list[[i]] %>% dplyr::select(-taxon_num)
+# }
+# 
+# # create list of taxon-specific dataframes within time-specific list items
+# dat_list <- lapply(dat_list, function(x) split(x, f = x$taxon))
+# for(i in 1:n_times){
+#   dat_list[[i]] <- lapply(dat_list[[i]], 
+#                            function(x) x[!(names(x) %in% c('taxon'))])
+# }
 
 #### PREPARE MEAN PREDICTION DATAFRAMES ####
 # create list of dataframes, grouped by time period
@@ -252,3 +253,144 @@ for (i in 1:n_times){
   print(p)
 }
 dev.off()
+
+
+##############################################################
+#### PLOT RAW DATA VS. PREDICTIONS AT POLLEN RECORD LOCATIONS
+##############################################################
+
+# read raw data
+rel_prop <- readRDS('data/rel_props_raw_data.RDS')
+
+# read taxon names; remove non-tree taxa; combine unused tree taxa counts into 'other' category
+# version <- 'Dec15'
+dat <- readRDS(paste0('output/polya-gamma-dat_', version, '.RDS'))
+taxa <- dat$taxa.keep
+
+end_num <- which(colnames(rel_prop) == 'dataset')
+other_cols <- colnames(rel_prop[,1:end_num])
+taxa_nontree <- c('Other', 'Prairie.Forbs', 'Poaceae')
+other_tree <- colnames(rel_prop[!(colnames(rel_prop) %in% taxa) & !(colnames(rel_prop) %in% taxa_nontree) & 
+                  !(colnames(rel_prop) %in% other_cols)])
+
+rel_prop$Other_tree <- apply(rel_prop[,colnames(rel_prop) %in% other_tree], 1, function(x) sum(x, na.rm=TRUE))
+
+# remove unneeded taxa
+keep <- c(colnames(rel_prop[,1:end_num]), taxa, 'Other_tree')
+rel_prop <- rel_prop[ , colnames(rel_prop) %in% keep]
+colnames(rel_prop)[colnames(rel_prop) == 'Other_tree'] <- 'Other'
+
+# create time bins
+time_bins <- c(min(rel_prop$age), seq(0, 21000, by=990))
+n_times <- length(time_bins)-1
+
+age_cut <- cut(rel_prop$age, include.lowest = TRUE, breaks = time_bins)
+rel_prop$cut <- as.integer(age_cut)
+rel_prop <- rel_prop[!is.na(rel_prop$cut),] # remove rows with records >21k years old
+
+# separate dataframe into list of dataframes, one for each time chunk, remove excess columns
+time <- split(rel_prop, f = rel_prop$cut)
+keep <- c('x','y', taxa, 'Other')
+time <- lapply(time, function(x) x[names(x) %in% keep])
+
+# sum pollen counts by time chunk/site
+time_sum <- list()
+for(i in 1:n_times){
+  time_sum[[i]] <- time[[i]] %>% group_by(x, y) %>% summarise_all(.funs = sum, na.rm = TRUE)
+  time_sum[[i]] <- ungroup(time_sum[[i]])
+  time_sum[[i]]$time <- i
+}
+
+# rowbind to create one dataframe
+rel_prop_df <- bind_rows(time_sum)
+# saveRDS(rel_prop_df, 'data/rel_prop_df_for_plotting.RDS')
+
+
+# read prediction grid locations
+locs_grid <- readRDS(paste0('data/grid_', version, '.RDS'))
+
+# read mean preds (or median)
+means <- readRDS(paste0('output/preds_', version, '_all_taxa_means.RDS'))
+n_times <- dim(means)[3]
+n_taxa <- dim(means)[2]
+taxa <- c(taxa, 'Other')
+
+preds_list <- list()
+for(i in 1:n_times){
+  preds_list[[i]] <- cbind(locs_grid, data.frame(means[,,i]))
+  colnames(preds_list[[i]])[3:ncol(preds_list[[i]])] <- taxa
+  preds_list[[i]]$time <- i
+}
+
+preds_df <- bind_rows(preds_list)
+preds_df <- pivot_longer(preds_df, taxa, 
+                         names_to = 'taxon', values_to = 'pred')
+# saveRDS(preds_df, 'data/preds_df_for_plotting.RDS')
+
+
+#### DETERMINE WHICH PREDICTIONS ARE CLOSEST TO POLLEN RECORD LOCATIONS FOR FRAXINUS
+# require(rgeos)
+# preds_df <- readRDS('data/preds_df_for_plotting.RDS')
+# rel_prop_df <- readRDS('data/rel_prop_df_for_plotting.RDS')
+
+proj <- "+proj=aea +lat_0=37.5 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"
+preds_frax <- preds_df[preds_df$taxon == "Fraxinus",]
+rel_prop_frax <- rel_prop_df[,c('x','y','Fraxinus','time')]
+rel_prop_frax <- rel_prop_frax[rel_prop_frax$Fraxinus > 0,]
+n_times <- nrow(preds_frax %>% dplyr::select(time) %>% distinct())
+
+prop <- list()
+preds <- list()
+for(i in 1:n_times){
+prop[[i]] <- rel_prop_frax[rel_prop_frax$time == i, ]
+preds[[i]] <- preds_frax[preds_frax$time == i, ]
+
+sp_rel_prop <- SpatialPointsDataFrame(coords = prop[[i]][,c('x','y')],
+                                      data = prop[[i]][,3:ncol(prop[[i]])],
+                                      proj4string = CRS(proj))
+sp_preds <- SpatialPointsDataFrame(coords = preds[[i]][,c('x','y')],
+                                   data = preds[[i]][,3:ncol(preds[[i]])],
+                                   proj4string = CRS(proj))
+prop[[i]]$nearest <- apply(gDistance(sp_preds, sp_rel_prop, byid = TRUE), 1, which.min)
+preds[[i]]$nearest <- as.integer(rownames(preds[[i]]))
+prop[[i]] <- left_join(prop[[i]], preds[[i]][,c('pred','nearest')], by = 'nearest')
+
+}
+
+prop_preds <- bind_rows(prop)
+
+prop1 <- rel_prop_frax[rel_prop_frax$time == 1, ]
+preds1 <- preds_frax[preds_frax$time == 1, ]
+
+sp_rel_prop <- SpatialPointsDataFrame(coords = prop1[,c('x','y')],
+                                      data = prop1[,3:ncol(prop1)],
+                                      proj4string = CRS(proj))
+sp_preds <- SpatialPointsDataFrame(coords = preds1[,c('x','y')],
+                                   data = preds1[,3:ncol(preds1)],
+                                   proj4string = CRS(proj))
+prop1$nearest <- apply(gDistance(sp_preds, sp_rel_prop, byid = TRUE), 1, which.min)
+
+preds1$nearest <- as.integer(rownames(preds1))
+prop1 <- left_join(prop1, preds1[,c('pred','nearest')], by = 'nearest')
+
+
+
+# plot preds and props together to make sure code worked
+ggplot() +
+  geom_path(data = cont_shp, aes(x = long, y = lat, group = group)) +
+  geom_path(data = lake_shp, aes(x = long, y = lat, group = group)) +
+  
+  geom_point(data = preds1[preds1$nearest %in% prop1$nearest,], aes(x = x, y = y),
+             alpha = 0.5, size = 3, color = 'red') +
+  geom_point(data = prop1, aes(x = x, y = y), alpha = 0.5, size = 2, color = 'blue') +
+  
+  scale_y_continuous(limits = ylim) +
+  scale_x_continuous(limits = xlim) +
+  theme_classic() +
+  coord_equal()
+
+plot(prop1$Fraxinus, prop1$pred, 
+     xlab = 'Fraxinus relative proportion',
+     ylab = 'Fraxinus model predictions',
+     main = 'Present')
+
